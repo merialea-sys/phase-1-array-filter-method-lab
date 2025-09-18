4ae70fd2b0a91d163e2c4a198cfda6aad91963ac
merialea <merialea5@gmail.com>
2025-09-18 12:43:27 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby'];
+
+function findMatching(drivers ,searchString ){
+    const driver = drivers.filter(drivers=> drivers.toLowerCase()== searchString.toLowerCase());
+    return driver;
+}
+
+function fuzzyMatch(drivers, query){
+    const lowerCaseQuery= query.toLowerCase();
+    const driverFirstInitial = drivers.filter(drivers =>drivers.toLowerCase().startsWith(lowerCaseQuery));
+    return driverFirstInitial;
+}
+
+
+    const driver = [
+        {
+          name: 'Bobby',
+          hometown: 'Pittsburgh' },
+        {
+          name: 'Sammy',
+          hometown: 'New York' } ,
+        {
+          name: 'Sally',
+          hometown: 'Cleveland' },
+        {
+          name: 'Annette',
+          hometown: 'Los Angeles' },
+        {
+          name: 'Bobby',
+          hometown: 'Tampa Bay' }
+      ];
+
+function matchName(driver, searchString){
+    const nameOfDriver = driver.filter(driver => driver.name === searchString);
+    return nameOfDriver;
+}
